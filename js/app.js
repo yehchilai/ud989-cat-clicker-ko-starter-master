f48bce29ef3d7b5273c7e0aa962b29c28cdb64db
Yeh-chi Lai <yehchilai@csupomona.edu>
2017-01-04 15:12:28 -0800

    add: list, model(cat)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-var ViewModel = function(){
+var Cat = function(){
   var self = this;
   self.count = ko.observable(0);
   self.name = ko.observable("Taddy");
   self.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
 
-  self.increamentCounter = function(){
-    self.count(self.count() + 1);
-  };
+  self.nickname = ko.observableArray([
+    {name:'Tad'},
+    {name:'TaTa'},
+    {name:'dydy'}
+  ]);
 
   self.level = ko.computed(function(){
     if(self.count() === 0){
@@ -19,7 +21,14 @@ var ViewModel = function(){
       return "Adult";
     }
   });
+}
 
+var ViewModel = function(){
+  var self = this;
+  self.currentCat = ko.observable(new Cat());
+  self.increamentCounter = function(){
+    self.currentCat().count(self.currentCat().count() + 1);
+  };
 }
 
 ko.applyBindings(new ViewModel());
