d32010b23c1010188fdca3c17f5d2a96e8575b8d
Yeh-chi Lai <yehchilai@csupomona.edu>
2017-01-04 16:08:37 -0800

    add - list with click function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,39 @@
-var Cat = function(){
-  var self = this;
-  self.count = ko.observable(0);
-  self.name = ko.observable("Taddy");
-  self.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
+var cats = [
+  {
+    count: 0,
+    name: "Taddy",
+    imgSrc: "img/22252709_010df3379e_z.jpg",
+    nicknames :['Tad','TaTa','dydy']
+  },
+  {
+    count: 0,
+    name: "Kiger",
+    imgSrc: "img/4154543904_6e2428c421_z.jpg",
+    nicknames :['KiKi','gerger','Kit']
+  },
+  {
+    count: 0,
+    name: "Lazzer",
+    imgSrc: "img/9648464288_2516b35537_z.jpg",
+    nicknames :['Laz','LaLa','zer']
+  }
 
-  self.nickname = ko.observableArray([
-    {name:'Tad'},
-    {name:'TaTa'},
-    {name:'dydy'}
-  ]);
+];
+
+
+var Cat = function(data){
+  var self = this;
+  self.count = ko.observable(data.count);
+  self.name = ko.observable(data.name);
+  self.imgSrc = ko.observable(data.imgSrc);
+
+  self.nickname = ko.observableArray(data.nicknames
+  // [
+    // {name:'Tad'},
+    // {name:'TaTa'},
+    // {name:'dydy'}
+  // ]
+  );
 
   self.level = ko.computed(function(){
     if(self.count() === 0){
@@ -25,9 +50,20 @@ var Cat = function(){
 
 var ViewModel = function(){
   var self = this;
-  self.currentCat = ko.observable(new Cat());
+
+  self.catList = ko.observableArray([]);
+  cats.forEach(function(catItem){
+    self.catList.push(new Cat(catItem));
+  });
+  // console.log(self.catList());
+  self.currentCat = ko.observable(self.catList()[0]);
+
   self.increamentCounter = function(){
     self.currentCat().count(self.currentCat().count() + 1);
+  };
+
+  self.selectCat = function(cat){
+    self.currentCat(cat);
   };
 }
 
